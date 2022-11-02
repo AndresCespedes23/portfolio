18bcf71ef0cffd596c231e45cc420198c29dd7a3
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-11-02 10:53:15 -0300

    change the emoji

--- src/Components/Shared/Header/index.js
+++ src/Components/Shared/Header/index.js
@@ -7,7 +7,7 @@ import styles from "./header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>🛸</div>
+      <div className={styles.logo}>👨🏼‍🚀</div>
       <nav>
         <ul>
           <Link className={styles.navLinks} to="/">
